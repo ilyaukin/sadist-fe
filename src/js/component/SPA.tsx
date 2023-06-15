63d45391f0c8d29c6dcec2d091e37d738b4578e0
Ilya Lyaukin <kzerby@gmail.com>
2023-06-15 15:53:17 -0600

    SPA simplified a little bit

--- src/js/component/SPA.tsx
+++ src/js/component/SPA.tsx
@@ -18,7 +18,7 @@ let SPA = () => {
   const [ds, setDs] = React.useState<any[]>([]);
   const [dsInfo, dispatchDsInfo] = React.useReducer(reduceDsInfo, defaultDsInfo);
   const [tableContentHeight, setTableContentHeight] = React.useState(
-    Math.min(300, Math.max(100, Math.floor(window.innerHeight / 3))));
+      Math.min(300, Math.max(100, Math.floor(window.innerHeight / 3))));
   const [vizHeight, setVizHeight] = React.useState<number | undefined>();
 
   ErrorDialog.raise = setErr;
@@ -26,44 +26,40 @@ let SPA = () => {
 
   const vizRef = useRef<VizElement | null>(null);
 
-  const getTitle = function () {
-    const titles = [
-      'My handicapped pet project....',
-      'Let steal the beggars!',
-    ];
-    const choose = Math.floor(Math.random() * titles.length);
-    return titles[choose];
-  };
+  const titles = [
+    'My handicapped pet project....',
+    'Let steal the beggars!',
+  ];
+  const choose = Math.floor(Math.random() * titles.length);
+  const title = titles[choose];
 
-  const renderViz = function () {
-    if (!dsInfo.meta.id) {
-      return null;
-    }
-
-    return <>
-      <Splitter onSplit={(delta: number) => setTableContentHeight(tableContentHeight + delta)}/>
+  let viz = null;
+  if (dsInfo.meta.id) {
+    viz = <>
+      <Splitter
+          onSplit={(delta: number) => setTableContentHeight(tableContentHeight + delta)}/>
 
       <h2>2. Visualize</h2>
       <Viz
-        ref={vizRef}
-        vizHeight={vizHeight}
-        dsId={dsInfo.meta.id}
-        dsInfo={dsInfo}
-        dispatchDsInfo={dispatchDsInfo}
+          ref={vizRef}
+          vizHeight={vizHeight}
+          dsId={dsInfo.meta.id}
+          dsInfo={dsInfo}
+          dispatchDsInfo={dispatchDsInfo}
       />
       <Splitter onSplit={(delta: number) => {
         const h = vizHeight || vizRef.current?.element?.getBoundingClientRect().height || 0;
         setVizHeight(h + delta);
       }}/>
     </>;
-  };
+  }
 
   return <div className="content">
     <ErrorDialog err={err}/>
 
     <UserDropdown/>
     <h1>
-      {getTitle()}
+      {title}
     </h1>
     <wired-divider/>
 
@@ -75,15 +71,15 @@ let SPA = () => {
     })}/>
     {/*show top from selected ds*/}
     <DsTable
-      tableContentHeight={tableContentHeight}
-      dsId={dsInfo.meta.id}
-      dsInfo={dsInfo}
-      dispatchDsInfo={dispatchDsInfo}
-      onLoadDs={setDs}
-      ds={ds}
+        tableContentHeight={tableContentHeight}
+        dsId={dsInfo.meta.id}
+        dsInfo={dsInfo}
+        dispatchDsInfo={dispatchDsInfo}
+        onLoadDs={setDs}
+        ds={ds}
     />
 
-    {renderViz()}
+    {viz}
   </div>;
 };
 
