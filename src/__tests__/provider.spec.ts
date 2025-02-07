bee0dc24e71e2be8768ec6593951a2525cf80513
Ilya Lyaukin <kzerby@gmail.com>
2025-02-07 14:09:44 -0600

    w.i.p. testing  * move components tests to the components directory  * upgrade webpack-dev-server  * mock sadist-proxy  * add Playwright tests

---
+++ src/__tests__/provider.spec.ts
@@ -0,0 +1,62 @@
+import { expect, Frame, test } from '@playwright/test';
+
+let onWebpackOverlay = (frame: Frame) => {
+  let promise = frame
+      .$('#webpack-dev-server-client-overlay-div')
+      .then((element) => element?.innerText())
+      .then((content) => {
+        if (typeof content === 'string' && content.includes('Uncaught runtime errors:\n')) {
+          throw new Error(content);
+        }
+      }, () => {
+        /* not a webpack error frame */
+      });
+  onWebpackOverlayPromise.push(promise);
+};
+
+let onWebpackOverlayPromise: any[] = [];
+
+test.beforeEach(({ page }) => {
+  page.on('frameattached', onWebpackOverlay);
+});
+
+test.afterEach(async ({ page }) => {
+  await Promise.all(onWebpackOverlayPromise);
+  page.off('frameattached', onWebpackOverlay);
+})
+
+test('test', async ({ page }) => {
+
+  await page.goto('/');
+  await page.getByText('Choose data source...').click();
+  await page.getByRole('button', { name: '[+]New' }).click();
+  await page.locator('#source-type #textPanel').click();
+  await page.getByRole('button', { name: 'WebWeb Crawler' }).click();
+  await page.getByRole('textbox').click();
+  await page.getByRole('textbox').fill('http://example.com');
+  await page.getByRole('textbox').press('Tab');
+  await page.locator('wired-combo-lazy').press('ArrowDown');
+  await page.locator('wired-combo-lazy #searchInput').press('Enter');
+  await page.getByRole('button', { name: 'Next' }).click();
+
+  // Remember right block width
+  const w0 = await page.locator('.site-html-tree-block').evaluate(
+      (element) => element.clientWidth,
+  );
+
+  // Click vertical divider
+  await page.locator('.new-dialog .dialog-content div > div:nth-child(2) wired-divider').first().dblclick();
+
+  // New size
+  const w1 = await page.locator('.site-html-tree-block').evaluate(
+      (element) => element.clientWidth,
+  );
+  expect(w1).toBeGreaterThan(w0);
+
+  // Bug: it crawls to the right indefinitely...
+  await page.waitForTimeout(500);
+  const w2 = await page.locator('.site-html-tree-block').evaluate(
+      (element) => element.clientWidth,
+  );
+  expect(w2).toEqual(w1);
+});
