8b276b320cf3b5271e071b327a0a499c212e6da1
Ilya Lyaukin <kzerby@gmail.com>
2025-03-07 08:21:20 -0600

    add tests on PR

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
+    command: 'npm run start:no-open',
     url: 'http://localhost:8080/',
     reuseExistingServer: !process.env.CI,
   },
